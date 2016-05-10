86bb4f6f65d7fb3c477a5e0c5afa2b2780e24934
Maxim Pervushin <maxim.pervushin@gmail.com>
2016-05-10 15:58:47 +0300

    Added Fastlane/Snapshot

---
+++ Overlap/Snapshots/OverlapSnapshots.swift
@@ -0,0 +1,39 @@
+//
+//  OverlapSnapshots.swift
+//  OverlapSnapshots
+//
+//  Created by Maxim Pervushin on 10/05/16.
+//  Copyright © 2016 Maxim Pervushin. All rights reserved.
+//
+
+import XCTest
+
+class OverlapSnapshots: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        continueAfterFailure = false
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func test_MakeSnapshots() {
+        snapshot("01_Start")
+        
+        let app = XCUIApplication()
+        app.tables.staticTexts["Add Overlap"].tap()
+        
+        snapshot("02_Editor")
+        
+        app.navigationBars["Overlap.OverlapEditorView"].buttons["Save"].tap()
+
+        snapshot("03_List")
+    }
+    
+}
